fbeaa1fdd7314a62a1cd9686937e3d21efd425ba
Robin Vize <robin.vize@unipartrail.com>
2022-06-21 14:17:44 +0100

    name main stop sphinx executing it

--- {{cookiecutter.project_slug}}/src/data/create_folds.py
+++ {{cookiecutter.project_slug}}/src/data/create_folds.py
@@ -1,22 +1,23 @@
 import pandas as pd
 from sklearn.model_selection import StratifiedKFold
 
-print("Loading data")
-mnist_train_df = pd.read_csv("../../data/raw/mnist_train.csv")
+if __name__ == "__main__":
+    print("Loading data")
+    mnist_train_df = pd.read_csv("../../data/raw/mnist_train.csv")
 
-X = mnist_train_df.drop("label", axis=1)
-y = mnist_train_df.loc[:,'label']
+    X = mnist_train_df.drop("label", axis=1)
+    y = mnist_train_df.loc[:,'label']
 
-print("Creating fold object")
-skf = StratifiedKFold(n_splits=10, random_state=24, shuffle=True)
+    print("Creating fold object")
+    skf = StratifiedKFold(n_splits=10, random_state=24, shuffle=True)
 
-print("Assigning folds")
-fold = 1
-mnist_train_df["kfold"] = ""
-for train_index, test_index in skf.split(X, y):
-    mnist_train_df.loc[test_index,:] = mnist_train_df.loc[test_index,:].assign(kfold=fold)
-    fold += 1
+    print("Assigning folds")
+    fold = 1
+    mnist_train_df["kfold"] = ""
+    for train_index, test_index in skf.split(X, y):
+        mnist_train_df.loc[test_index,:] = mnist_train_df.loc[test_index,:].assign(kfold=fold)
+        fold += 1
 
-print("Writing to csv")
-filepath = "../../data/processed/mnist_train_folds.csv"
-mnist_train_df.to_csv(filepath) 
+    print("Writing to csv")
+    filepath = "../../data/processed/mnist_train_folds.csv"
+    mnist_train_df.to_csv(filepath) 
