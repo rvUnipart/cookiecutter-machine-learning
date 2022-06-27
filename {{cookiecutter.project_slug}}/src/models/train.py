1ab7a9e0f6bdb4f7dd994c975a48e6b986aca757
Robin Vize <robin.vize@unipartrail.com>
2022-06-27 19:04:13 +0100

    mlflow code merge

--- {{cookiecutter.project_slug}}/src/models/train.py
+++ {{cookiecutter.project_slug}}/src/models/train.py
@@ -1,9 +1,14 @@
 import argparse
+import warnings
 
 import joblib
 import pandas as pd
 from sklearn import metrics
 from sklearn import tree
+
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
 
 import config
 import model_dispatcher
@@ -30,21 +35,42 @@ def run(fold, model):
 
     joblib.dump(clf, f"{config.MODEL_OUTPUT}/dt_{fold}.bin")
 
+    return accuracy
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+    with mlflow.start_run():
+        warnings.filterwarnings("ignore")
+        
+        parser = argparse.ArgumentParser()
 
-    parser.add_argument(
-        "--fold",
-        type=int
-    )
-    parser.add_argument(
-        "--model",
-        type=str
-    )
+        parser.add_argument(
+            "--fold",
+            type=int
+        )
+        parser.add_argument(
+            "--model",
+            type=str
+        )
 
-    args = parser.parse_args()
+        args = parser.parse_args()
 
-    run(
-        fold=args.fold,
-        model=args.model
-    )
+        accuracy = run(
+            fold=args.fold,
+            model=args.model
+        )
+
+        mlflow.log_param("fold", args.fold)
+        mlflow.log_param("model", args.model)
+        mlflow.log_metric("accuracy", accuracy)
+
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+        # Model registry does not work with file store
+        if tracking_url_type_store != "file":
+            # Register the model
+            # There are other ways to use the Model Registry, which depends on the use case,
+            # please refer to the doc for more information:
+            # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+            mlflow.sklearn.log_model(args.model, "model", registered_model_name="ModelNameGoesHere")
+        else:
+            mlflow.sklearn.log_model(args.model, "model")
