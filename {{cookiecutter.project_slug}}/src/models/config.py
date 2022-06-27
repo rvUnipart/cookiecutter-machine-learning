87cabd7ba3f99a76901ad8aba0382201205130fb
Robin Vize <robin.vize@unipartrail.com>
2022-06-27 18:19:54 +0100

    run everything from home dir

--- {{cookiecutter.project_slug}}/src/models/config.py
+++ {{cookiecutter.project_slug}}/src/models/config.py
@@ -1,3 +1,9 @@
-TRAINING_FILE = "../../data/processed/mnist_train_folds.csv"
+import os, sys
 
-MODEL_OUTPUT = "../../models"
+##TRAINING_FILE = "../../data/processed/mnist_train_folds.csv"
+TRAINING_FILE = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '../../data/processed/mnist_train_folds.csv'))
+
+##MODEL_OUTPUT = "../../models" # original relative approach
+MODEL_OUTPUT = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '../../models'))
+
+SAME_DIR = os.path.dirname(os.path.realpath(__file__))
