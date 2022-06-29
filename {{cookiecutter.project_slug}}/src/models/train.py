cf1bdc9de177bcbce4dcb644d8d4c752edfc6492
Robin Vize <robin.vize@unipartrail.com>
2022-06-29 11:44:19 +0100

    Update train.py

--- {{cookiecutter.project_slug}}/src/models/train.py
+++ {{cookiecutter.project_slug}}/src/models/train.py
@@ -60,9 +60,4 @@ if __name__ == "__main__":
         mlflow.log_params(clf_params)
         mlflow.log_metric("accuracy", accuracy)
 
-        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
-
-        if tracking_url_type_store != "file":
-            mlflow.sklearn.log_model(args.model, "model", registered_model_name="ModelNameGoesHere")
-        else:
-            mlflow.sklearn.log_model(args.model, "model")
+        mlflow.sklearn.log_model(sk_model=args.model, artifact_path="sklearn-model")
