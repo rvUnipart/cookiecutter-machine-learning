8a117a202f493c9ed0864e761b33c1ca24fc54c5
Robin Vize <robin.vize@unipartrail.com>
2022-07-08 11:44:11 +0100

    last checkpoint before pipelines move
    note: did not check these working before moving to mlflow pipelines but probably won't go back to this point now anyway

---
+++ {{cookiecutter.project_slug}}/src/register.py
@@ -0,0 +1,7 @@
+import os, sys
+
+RAW_DATA_FILE = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '../data/raw/mnist_train.csv'))
+
+TRAINING_FILE = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '../data/processed/mnist_train_folds.csv'))
+
+SAME_DIR = os.path.dirname(os.path.realpath(__file__))
